bb61ccc4fd00076597a9021470074e4b9aea29a5
balegas <v.sousa@campus.fct.unl.pt>
2014-11-04 02:49:14 +0000

    Changed start time output format

--- src/client_stats.erl
+++ src/client_stats.erl
@@ -44,24 +44,24 @@ receive
     case Status of
       success ->
         io:fwrite(File, "~p\t~p\t~p\t~p\t~p\t~p\t~p\t~p\t~n", 
-                  [Pid,Key,Value, Permissions, Latency, Timestamp, Operation, Status]),
+                  [Pid,Key,Value, Permissions, Latency, now_microsec(Timestamp), Operation, Status]),
         NewBins = orddict:update(timer:now_diff(Timestamp,StartTime) div ?PLOT_INTERVAL,
           fun({Sum,Count}) -> {Sum+Latency,Count+1} end, {0,0}, Bins),
         stats(NewBins,Success+1,Fail,Forbidden,Finished, min(Value,MinValue),
               StartTime,Running,Total,File,ParentPid);
       failure ->
         io:fwrite(File, "~p\t~p\t~p\t~p\t~p\t~p\t~p\t~p\t~n", 
-                  [Pid,Key,Value, Permissions, Latency, Timestamp, Operation, Status]),
+                  [Pid,Key,Value, Permissions, Latency, now_microsec(Timestamp), Operation, Status]),
         stats(Bins,Success,Fail+1,Forbidden,Finished, min(Value,MinValue),
               StartTime,Running,Total,File,ParentPid);
       forbidden ->
         io:fwrite(File, "~p\t~p\t~p\t~p\t~p\t~p\t~p\t~p\t~n", 
-                  [Pid,Key,Value, Permissions, Latency, Timestamp, Operation, Status]),
+                  [Pid,Key,Value, Permissions, Latency, now_microsec(Timestamp), Operation, Status]),
         stats(Bins,Success,Fail,Forbidden+1,Finished, min(Value,MinValue),
               StartTime,Running,Total,File,ParentPid);
       finished ->
         io:fwrite(File, "~p\t~p\t~p\t~p\t~p\t~p\t~p\t~p\t~n", 
-                  [Pid,Key,Value, Permissions, Latency, Timestamp, Operation, Status]),
+                  [Pid,Key,Value, Permissions, Latency, now_microsec(Timestamp), Operation, Status]),
         stats(Bins,Success,Fail,Forbidden,Finished+1, min(Value,MinValue),
               StartTime,Running,Total,File,ParentPid)
     end;
@@ -92,3 +92,7 @@ print_output(Bins,Success,Fail,Forbidden,Finished,StartTime,Total) ->
             [Success,Fail,Forbidden,Finished]),
   io:format("N_CLIENTS:~p~nDURATION:~p seconds ~n",
             [Total,timer:now_diff(now(),StartTime)/math:pow(10,6)]).
+
+now_microsec({MegaSecs, Secs, MicroSecs}) ->
+    (MegaSecs * 1000000 + Secs) * 1000000 + MicroSecs.
+
